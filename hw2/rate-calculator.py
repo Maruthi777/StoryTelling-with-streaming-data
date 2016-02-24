a1ba01cc0524e135e06468323f642931dc7db03a
Maruthi777 <msv2130@columbia.edu>
2016-02-24 16:12:16 -0500

    calculate rate from Redis database

--- hw2/rate-calculator.py
+++ hw2/rate-calculator.py
@@ -1,28 +1,30 @@
+# In this program, we will calculate the rate by getting values from Redis
+
 import redis
 import json
 import time
 import sys
 
-connection = redis.Redis()
+connection = redis.Redis() #establish connection
 
 while 1:
 
-    keys = connection.keys()
+    keys = connection.keys() #get keys
 
-    data = connection.mget(keys)
+    data = connection.mget(keys) #get all data
 
-    try:
-        time_difference = [float(v) for v in data]
-    except TypeError:
+    try: #so that no unexpected errors arise
+        time_difference = [float(v) for v in data] 
+    except TypeError: #so that no unexpected errors arise
         print keys
         continue
 
-    if len(time_difference):
+    if len(time_difference): #calculate rate
         rate = sum(time_difference)/float(len(time_difference))
     else:
         rate = 0
 
-    print json.dumps({"the rate is":rate})
-    sys.stdout.flush()
+    print json.dumps({"the rate is":rate}) #print rate
+    sys.stdout.flush() # flush to stdout
 
-    time.sleep(0.5)
+    time.sleep(0.5) #sleep
