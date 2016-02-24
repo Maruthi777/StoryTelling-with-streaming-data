205f19fa014c9dcb55deb3fe783a61e06a8cbc8e
Maruthi777 <msv2130@columbia.edu>
2016-02-24 16:10:13 -0500

    difference in probability calculated

--- hw2/diff-calculate.py
+++ hw2/diff-calculate.py
@@ -1,12 +1,15 @@
+# In this file, we get the std out that has been flushed from randomtoss.py. The std out is 
+# in JSON format and contains information regarding obtained ratio. We use this information to 
+# calculate diff between actual and expected ratios and flush it to std out.
 import json
 import sys
 import time
 
-last = 0
-while 1:
-    prev = sys.stdin.readline()
-    input = json.loads(prev)
-    difference = abs(input["ratio"] - 0.1)
+while 1: #repeat forever
+    prev_output = sys.stdin.readline() #read from stdin
+    input = json.loads(prev_output) #load data in "input"
+    difference = abs(input["ratio"] - 0.1) #calculate the difference
     t = time.time()
-    print json.dumps({"difference in the ratio":difference,"actual ratio":input["ratio"],"time":t})
-    sys.stdout.flush()
+    print json.dumps({"difference in the ratio":difference,"actual ratio":input["ratio"],"time":t}) #print in JSON format the difference in ratios, actual ratio and timestamp at which it was obtained
+    sys.stdout.flush() #flush to std out
+    
