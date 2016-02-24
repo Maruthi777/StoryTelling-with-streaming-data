3283088cf9e3f6ba139dea3c61d217aaa2546847
Maruthi777 <msv2130@columbia.edu>
2016-02-24 16:11:38 -0500

    pipe to the Redis database

--- hw2/pipe-to-redis.py
+++ hw2/pipe-to-redis.py
@@ -1,16 +1,16 @@
+# In this program, we will connect to the Redis database and load values in the database
 import json
 import sys
 import redis
 import time
 
-connection=redis.Redis()
+connection=redis.Redis() #establish connection. 
 
 while True:
     reading = sys.stdin.readline()
     input = json.loads(reading)
-    time_difference = input["delta_time"]
-    t = input["time"]
-    #print difference," 1 "
-    connection.setex(t, time_difference, 120)
-    print json.dumps({"time difference":time_difference,"time":t})
-    sys.stdout.flush()
+    time_difference = input["delta_time"] #obtain values of delta_time and 
+    t = input["time"] # time
+    connection.setex(t, time_difference, 100) # setting value of t to be time_difference for 100 seconds.
+    print json.dumps({"time difference":time_difference,"time":t}) #print difference and time.
+    sys.stdout.flush() #flush to stdout
