41d49cdfce5752878fdfce92a6335bf2da06d80e
Maruthi777 <msv2130@columbia.edu>
2016-02-24 17:37:37 -0500

    pipe to the Redis database

--- hw2/pipe-to-redis.py
+++ hw2/pipe-to-redis.py
@@ -11,6 +11,6 @@ while True:
     input = json.loads(reading)
     time_difference = input["delta_time"] #obtain values of delta_time and 
     t = input["time"] # time
-    connection.setex(t, time_difference, 100) # setting value of t to be time_difference for 100 seconds.
+    connection.setex(t,time_difference,100) # setting value of t to be time_difference for 100 seconds.
     print json.dumps({"time difference":time_difference,"time":t}) #print difference and time.
     sys.stdout.flush() #flush to stdout
